1f88e71b8787a2223f3376f33fb18a26b5ae6153
yuri.algoritmus <yuri@algoritmus.com.br>
2020-01-12 10:59:48 -0300

    Add login and logout views and templates

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# LOGIN
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
